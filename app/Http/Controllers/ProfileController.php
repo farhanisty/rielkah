a2942893a509c106d0ea823d86f03fa538885d47
Farhannivta <farhannivtasija@gmail.com>
2023-09-30 21:33:58 +0700

    add toggle view list

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -6,10 +6,17 @@ use Illuminate\Http\Request;
 
 class ProfileController extends Controller
 {
-  public function index()
+  public function index(Request $request)
   {
+    $view = $request->input('view');
+    
+    if($view !== "sort") {
+      $view = "grid";
+    }
+
     return view('pages.profile', [
-      'page' => 'profile'
+      'page' => 'profile',
+      'view' => $view
     ]);
   }
 }
--- resources/views/pages/profile.blade.php
+++ resources/views/pages/profile.blade.php
@@ -23,8 +23,8 @@
 
   <section class="border-t-2 border-slate-200 mt-10">
     <ul class="flex justify-around mt-5">
-      <li class="cursor-pointer pb-3 border-b-2 border-slate-300"><img src="{{ asset('assets/icons/pixels.png') }}" alt="grid" width="25" height="25" /></li>
-      <li><img src="{{ asset('assets/icons/sort.png') }}" alt="grid" height="25" width="27" /></li>
+      <li class="cursor-pointer {{ ($view == 'grid') ? 'pb-3 border-b-2 border-slate-300' : '' }}"><a href="?view=grid"><img src="{{ asset('assets/icons/pixels.png') }}" alt="grid" width="25" height="25" /></a></li>
+      <li class="cursor-pointer {{ ($view == 'sort') ? 'pb-3 border-b-2 border-slate-300' : '' }}"><a href="?view=sort"><img src="{{ asset('assets/icons/sort.png') }}" alt="grid" height="25" width="27" /></a></li>
     </ul>
   </section>
 </div>
