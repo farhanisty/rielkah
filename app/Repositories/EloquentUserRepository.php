363b8a9cb6e306c015d2a8a7dda145c41bf86735
Farhannivta <farhannivtasija@gmail.com>
2023-09-30 23:11:25 +0700

    using query to determine return of UserStatsDto

--- app/Repositories/EloquentUserRepository.php
+++ app/Repositories/EloquentUserRepository.php
@@ -3,12 +3,42 @@
 namespace App\Repositories;
 
 use App\Dto\UserStatsDto;
+use App\Models\User;
+use App\Models\FollowManagement;
 use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Query\JoinClause;
+use Illuminate\Support\Facades\DB;
 
 class EloquentUserRepository implements UserRepository
 {
   public function getWithStatsWhereId(int $id): UserStatsDto
   {
-    return new UserStatsDto('Farhannivta Ramadhana', 'farhannivta', 0, 1, 0);
+    $user = User::select('users.name', 'users.username', 'followers.follower', 'followed.followed')
+      ->leftJoinSub($this->createFollowersTable(), 'followers', function(JoinClause $join) {
+        $join->on('users.id', '=', 'followers.followed_id');
+      })
+      ->leftJoinSub($this->createFollowedTable(), 'followed', function(JoinClause $join) {
+        $join->on('users.id', '=', 'followed.user_id');
+      })
+      ->where('users.id', "=", $id)
+      ->first();
+
+    if(!$user) {
+      throw new Exception("Not Found");
+    }
+    
+    return new UserStatsDto($user->name, $user->username, 0, $user->follower ?? 0, $user->followed ?? 0);
+  }
+
+  private function createFollowersTable()
+  {
+    return FollowManagement::select('followed_id', DB::raw('count(user_id) as follower'))
+      ->groupBy('followed_id');
+  }
+
+  private function createFollowedTable()
+  {
+    return FollowManagement::select('user_id', DB::raw('count(followed_id) as followed'))
+      ->groupBy('user_id');
   }
 }
