35500ec0c04078832198af5d9278d6b555b42c00
Farhannivta <farhannivtasija@gmail.com>
2023-09-30 08:49:34 +0700

    create validation rule for Register user

---
+++ app/Http/Requests/RegisterPostRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+
+class RegisterPostRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        Validator::extend('without_spaces', function($attr, $value){
+            return preg_match('/^\S*$/u', $value);
+        });
+        
+        return [
+            'name' => 'required|min:4',
+            'username' => 'required|unique:users|min:6|max:32|lowercase|without_spaces',
+            'email' => 'required|unique:users|email:rfc,dns',
+            'password' => 'required|min:6'
+        ];
+    }
+}
