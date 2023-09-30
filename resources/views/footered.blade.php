6b272b71a00218625f83b92c23f55ed44a9a168f
Farhannivta <farhannivtasija@gmail.com>
2023-09-30 10:13:31 +0700

    add header is a part of footered view

--- resources/views/footered.blade.php
+++ resources/views/footered.blade.php
@@ -1,22 +1,24 @@
 @extends('layout')
 
 @section('body')
-    @yield('content')
-    
-    <div class="container fixed bg-white bottom-3 border-2 border-slate-300">
-      <footer class="min-h-[4rem] flex justify-around items-center">
-          <a href="{{ route('home.index') }}" class="{{ ($page == 'home') ? 'bg-blue-100' : '' }} p-2 px-3">
-            <img src="{{ asset('assets/icons/home.png') }}" width="30" height="30" />
-          </a>
-          <a href="{{ route('search.index') }}"class="{{ ($page == 'search') ? 'bg-blue-100' : '' }} p-2 px-3">
-            <img src="{{ asset('assets/icons/search.png') }}" width="30" height="30" />
-          </a>
-          <a href="{{ route('activity.index') }}" class="{{ ($page == 'activity') ? 'bg-blue-100' : '' }} p-2 px-3">
-            <img src="{{ asset('assets/icons/heart.png') }}" width="30" height="30" />
-          </a>
-          <a href="{{ route('profile.index') }}" class="{{ ($page == 'profile') ? 'bg-blue-100' : '' }} p-2 px-3">
-            <img src="{{ asset('assets/icons/user.png') }}" width="30" height="30" />
-          </a>
-      </footer>
-    </div>
+    <main class="w-100 flex justify-center py-5">
+        @yield('content')
+        
+        <div class="container fixed bg-white bottom-3 border-2 border-slate-300">
+          <footer class="min-h-[4rem] flex justify-around items-center">
+              <a href="{{ route('home.index') }}" class="{{ ($page == 'home') ? 'bg-blue-100' : '' }} p-2 px-3">
+                <img src="{{ asset('assets/icons/home.png') }}" width="30" height="30" />
+              </a>
+              <a href="{{ route('search.index') }}"class="{{ ($page == 'search') ? 'bg-blue-100' : '' }} p-2 px-3">
+                <img src="{{ asset('assets/icons/search.png') }}" width="30" height="30" />
+              </a>
+              <a href="{{ route('activity.index') }}" class="{{ ($page == 'activity') ? 'bg-blue-100' : '' }} p-2 px-3">
+                <img src="{{ asset('assets/icons/heart.png') }}" width="30" height="30" />
+              </a>
+              <a href="{{ route('profile.index') }}" class="{{ ($page == 'profile') ? 'bg-blue-100' : '' }} p-2 px-3">
+                <img src="{{ asset('assets/icons/user.png') }}" width="30" height="30" />
+              </a>
+          </footer>
+        </div>
+    </main>
 @endsection
