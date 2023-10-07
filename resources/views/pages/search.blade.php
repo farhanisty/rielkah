ff0016be12462dd9077023df31911de0a1441be9
Farhannivta <farhannivtasija@gmail.com>
2023-10-07 09:22:27 +0700

    create input search element

--- resources/views/pages/search.blade.php
+++ resources/views/pages/search.blade.php
@@ -3,11 +3,13 @@
 @section('content')
 <div class="container pb-[4rem]">
   <header class="w-full pb-3">
-      <h1 class="font-bold text-3xl uppercase">{{ env('APP_NAME', 'App') }}</h1>
+      <h1 class="font-bold text-3xl uppercase">Search</h1>
   </header>
 
-  <section class="w-full">
-      <h1>Hello world</h1>
+  <section class="w-full pt-3 md:pt-8">
+      <form class="flex gap-2">
+        <input class="border-2 p-2 flex-grow rounded" placeholder="search account..." type="text" />
+      </form>
   </section>
 </div>
 @endsection
