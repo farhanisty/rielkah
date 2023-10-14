722012b34eacb4ed5d74b7355c2fba98721b0ea6
Farhannivta <farhannivtasija@gmail.com>
2023-10-14 21:51:17 +0700

    perform edit user

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Repositories\UserRepository;
+use App\Http\Requests\EditProfileRequest;
+use App\Dto\EditableUser;
 
 class ProfileController extends Controller
 {
@@ -31,8 +33,28 @@ class ProfileController extends Controller
     ]);
   }
 
-  public function showEdit(Request $request)
+  public function showEdit()
   {
-    return view('pages.edit-profile');
+    $profile = $this->userRepository->getEditableUser(auth()->id());
+    return view('pages.edit-profile', [
+      'profile' => $profile
+    ]);
+  }
+
+  public function postEdit(EditProfileRequest $request)
+  {
+    $filename = null;
+    
+    if($request->file('profile_picture')) {
+      try{
+        $filename = $request->file('profile_picture')->store('profile-pictures');
+      }catch(Exception $exception) {
+        return redirect()->back();
+      }
+    }  
+
+    $this->userRepository->editUser(auth()->id(), new EditableUser($request->validated('name'), $filename));
+
+    return redirect()->route('profile.index');
   }
 }
