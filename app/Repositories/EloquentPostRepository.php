dbdc8328fa6457775b1955141ebb5700eafe5801
Farhannivta <farhannivtasija@gmail.com>
2023-10-16 09:13:48 +0700

    add getPostWhereId method

--- app/Repositories/EloquentPostRepository.php
+++ app/Repositories/EloquentPostRepository.php
@@ -2,8 +2,13 @@
 
 namespace App\Repositories;
 
+use App\Dto\PostDto;
 use App\Repositories\PostRepository;
+use App\Models\User;
 use App\Models\Post;
+use Carbon\Carbon;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\DB;
 
 class EloquentPostRepository implements PostRepository
 {
@@ -15,5 +20,21 @@ class EloquentPostRepository implements PostRepository
       'caption' => $caption,
       'created_at' => $createdAt
     ]);
+  }
+
+  public function getPostsWhereId(int $id): Collection
+  {
+    $userPosts = User::select('username', 'profile_picture', DB::raw('posts.id, posts.caption, posts.created_at, posts.image'))
+      ->join('posts', 'users.id', '=', 'posts.user_id')
+      ->where('users.id', '=', $id)
+      ->orderByRaw('posts.created_at desc')
+      ->get();
+
+    $posts = $userPosts->map(function($post, int $key) {
+      return new PostDto($post->username, $post->profile_picture, $post->image, $post->caption, Carbon::parse($post->created_at)->diffForHumans());
+    });
+
+
+    return $posts;
   }
 }
--- app/Repositories/PostRepository.php
+++ app/Repositories/PostRepository.php
@@ -2,7 +2,11 @@
 
 namespace App\Repositories;
 
+use Illuminate\Support\Collection;
+
 interface PostRepository
 {
   public function createPost(int $userId, string $image, string $caption, string $createdAt): void;
+
+  public function getPostsWhereId(int $id): Collection;
 }
