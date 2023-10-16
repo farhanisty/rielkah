01789970d924b4de366c9229b94e07acd2b1351b
Farhannivta <farhannivtasija@gmail.com>
2023-10-16 09:12:58 +0700

    create relation between post and user model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,10 +2,17 @@
 
 namespace App\Models;
 
+use App\Models\User;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
     use HasFactory;
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
