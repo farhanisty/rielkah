cb5f4a98ae5788254054d2e1e2202457ec42e3d1
Farhannivta <farhannivtasija@gmail.com>
2023-10-20 23:09:04 +0700

    create Comment Dto

--- app/Dto/Comment.php
+++ app/Dto/Comment.php
@@ -4,5 +4,13 @@ namespace App\Dto;
 
 class Comment
 {
-  
+  public function __construct(
+    public string $id,
+    public string $userId,
+    public string $postId,
+    public string $username,
+    public string $description,
+    public string $createdAt,
+    public ?string $replyId = null
+  ) { }
 }
