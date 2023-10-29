00b42f916902ecd62ea982002000aa923bcab540
Farhannivta <farhannivtasija@gmail.com>
2023-10-29 20:09:39 +0700

    isolate notification

---
+++ public/js/notificationMaker.js
@@ -0,0 +1,5 @@
+setTimeout(() => {
+  const notification = document.getElementById('notification');
+
+  notification.classList.add('-translate-y-[100px]');
+}, 3000)
--- resources/views/pages/post/show.blade.php
+++ resources/views/pages/post/show.blade.php
@@ -59,22 +59,16 @@
 @if(session('status') || $errors->any())
 @section('pusher')
 
-<div id="notification" class="fixed top-[20px] w-full flex justify-center duration-500" >
-  <div class="{{ $errors->any() ? 'bg-red-300' : 'bg-green-300' }} max-w-[250px] rounded-full py-2 px-5">
-    <p class="font-bold text-center capitalize text-white text-sm">{{ session('status') ?? $errors->first('description') }}</p>
-  </div>
-</div>
+  @php
+    $notificationDescription = session('status') ?? $errors->first('username')
+  @endphp
+  
+  @include('components/notification')
 
 @endsection
 
 @push('script')
-<script>
-setTimeout(() => {
-  const notification = document.getElementById('notification');
-
-  notification.classList.add('-translate-y-[100px]');
-}, 3000)
-</script>
+  <script src="{{ asset('js/notificationMaker.js') }}"></script>
 @endpush
 
 @endif
