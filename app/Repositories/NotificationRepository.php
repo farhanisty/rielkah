822c3908ac4eed8eef028d6fec74f4adfbfba39c
adit <madityanurdiansyah@gmail.com>
2023-11-16 15:17:21 +0700

    fix notification repository error

--- app/Repositories/NotificationRepository.php
+++ app/Repositories/NotificationRepository.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Collection;
 
 interface NotificationRepository
 {
-  public function createNotification(int $userId, string $link, string $type, string $createdAt, string $updatedAt, string $friendId): void;
+  public function createNotification(int $userId, string $link, string $type, string $createdAt, ?string $updatedAt, string $friendId): void;
 
   public function getNotificationsWhereUserId(int $id): Collection;
 }
